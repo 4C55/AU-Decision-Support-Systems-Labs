ad0d1a5b8d52d0ea5dddc362ae060de6876ffb62
Laurynas <laurynas.ubys@gmail.com>
2020-02-07 15:31:41 +0100

    Added smarket datset

---
+++ 4.6.1/main.py
@@ -0,0 +1,10 @@
+import Data
+
+def main():
+    data = Data.load_smarket_dataset()
+    print(data)
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- Data/Data.py
+++ Data/Data.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_boston
 import pandas as pd
+import os
 
 
 def load_boston_dataset():
@@ -7,3 +8,9 @@ def load_boston_dataset():
     boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
     boston['MEDV'] = boston_dataset.target
     return boston
+
+
+def load_smarket_dataset():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'Smarket.csv')
+    return pd.read_csv(filename, usecols=range(1, 10))
